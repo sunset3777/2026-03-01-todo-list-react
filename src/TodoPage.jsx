030836b38b36a854428419587dbd34b930f11368
Sunset3777 <aua52257@gmail.com>
2026-03-05 11:13:09 +0800

    feat:NavBar

--- src/TodoPage.jsx
+++ src/TodoPage.jsx
@@ -1,15 +1,11 @@
+import TodoNavbar from "../components/todo/TodoNavbar";
+
 function TodoPage () {
 
     return(
     <>
     <div id="todoListPage" className ="bg-half-yellow min-h-screen">
-        <nav className ="flex justify-between pt-6 px-8 mb-4 sm:mb-0">
-            <h1><a className ="block w-[243px] h-[39px] bg-[url('https://upload.cc/i1/2022/03/23/8vTzYG.png')] bg-no-repeat overflow-hidden whitespace-nowrap indent-[101%]" href="#">ONLINE TODO LIST</a></h1>
-            <ul className ="flex text-base">
-                <li className =" hidden sm:block"><a className ="no-underline text-[#333333] ml-0 sm:ml-6 hover:text-[#d87355]" href="#"><span className ="font-bold">王小明的代辦</span></a></li>
-                <li><a className ="no-underline text-[#333333] ml-0 sm:ml-6 hover:text-[#d87355]" href="#loginPage">登出</a></li>
-            </ul>
-        </nav>
+        <TodoNavbar />
         <div className ="h-screen mx-auto px-8 py-4">
             <div className ="w-full sm:w-[500px] mx-auto">
                 <div className ="w-full flex relative mb-8 shadow-md">
