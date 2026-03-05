498fec53fbbd7a08f3e577dea6151e2718d32d9e
Sunset3777 <aua52257@gmail.com>
2026-03-05 18:05:11 +0800

    fix:adjust vercel path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/2026-03-01-todo-list-react/",
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, ".", "VITE_");
+  const deployTarget = env.VITE_DEPLOY_TARGET; // "gh-pages" | "vercel"
+
+  return {
+    plugins: [react()],
+    base: deployTarget === "gh-pages" ? "/2026-03-01-todo-list-react/" : "/",
+  };
+});
