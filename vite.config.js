a4c09bad124651056e5a8c9ee2dbec071fd91849
Sunset3777 <aua52257@gmail.com>
2026-03-05 17:58:23 +0800

    fix: base path for vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/2026-03-01-todo-list-react/",
-})
+  base: "/",
+});
